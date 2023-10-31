350bb578523253e53a4261223c5048837bc14a36
prayushapandey04 <prayusha.pandey4@gmail.com>
2023-10-30 20:40:19 -0500

    added array of questions for user input in index.js file

--- index.js
+++ index.js
@@ -4,3 +4,35 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const shapes = require('./lib/shapes');
 
+// Array of questions for user input
+
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Please enter the text for your logo (note: can only be up to 3 characters.',
+        validate: (answer) => {
+            if (answer.length > 3) {
+                return console.log('Please make sure your text is no more than 3 characters');
+            } else {
+                return true;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'What color would you like the text of your logo to be?',
+    },
+    {
+        type: 'input',
+        name: 'logoShape',
+        message: 'What shape would you want your logo to be? Select from the options below',
+        choices: ['Cirlce, Square, Triange'],
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'What color would you want your shape to be?',
+    },
+];
